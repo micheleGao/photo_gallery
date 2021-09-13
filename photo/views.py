c4d382fbc13b88fd726ebb2bf7ee5f4eeacbc996
shellshellies <michele.gao.shellies@icloud.com>
2021-09-13 12:55:39 -0400

    added views

--- photo/views.py
+++ photo/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .serializers import ArtistSerializer, ReviewSerializer, GallerySerializer
+from .models import Artist, Gallery,Review
+from rest_framework import permissions
+from photo.permissions import IsOwnerOrReadOnly
 # Create your views here.
+class ArtistList(generics.ListCreateAPIView):
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
+class ArtistDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
+
+class GalleryList(generics.ListCreateAPIView):
+    queryset = Gallery.objects.all()
+    serializer_class = GallerySerializer
+
+class GalleryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Gallery.objects.all()
+    serializer_class = GallerySerializer
+
+class ReviewList(generics.ListCreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = [IsOwnerOrReadOnly]
